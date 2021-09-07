0d2a70ec942573eb84bcadd9fb8567dcb110c2de
mahdi kh <mahdikhodarahimi0@gmail.com>
2021-09-07 15:31:07 +0430

    Remove 123 start up

--- EndPoint.Site/Startup.cs
+++ EndPoint.Site/Startup.cs
@@ -51,7 +51,7 @@ namespace EndPoint.Site
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container. 123
+        // This method gets called by the runtime. Use this method to add services to the container. 
         public void ConfigureServices(IServiceCollection services)
         {
 
