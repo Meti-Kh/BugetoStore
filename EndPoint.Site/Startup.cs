95edddff200318058920072edcd1ef3d227c0042
MetiKh2 <90332147+MetiKh2@users.noreply.github.com>
2021-09-08 18:41:12 +0430

    Update Startup.cs

--- EndPoint.Site/Startup.cs
+++ EndPoint.Site/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
+//23eqdfe2
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using MyStore.Persistence.Context;
